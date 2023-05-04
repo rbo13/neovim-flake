40727ba042842890d7ae11a622fe0a09c7ea84be
rbo13 <chardyy.orofeo@gmail.com>
2023-05-04 11:17:36 +0800

    initial neovim configuration

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Neovim configuration as flake.";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    neovim = {
+      url = "github:neovim/neovim/stable?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs= { self, nixpkgs, neovim }: {
+    let
+      overlayFlakeInputs = prev: final: {
+        neovim = neovim.packages.x86_64-linux.neovim;
+      };
+
+      overlayNeovim = prev: final: {
+        neovim = import ./packages/neovim.nix {
+	  pkgs = final;
+	};
+      };
+
+      pkgs = {
+        system = "x86_64-linux";
+	overlays = [ overlayFlakeInputs overlayNeovim ];
+      };
+    
+    in {
+      packages.x86_64-linux.default = pkgs.neovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+	program = "${pkgs.neovim}/bin/nvim";
+      };
+    };
+
+  };
+}
